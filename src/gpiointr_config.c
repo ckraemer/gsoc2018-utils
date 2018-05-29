bc26c035aa6128fd9a00d688bf72f471dbc07d85
Christian Krämer <uddka@student.kit.edu>
2018-05-29 12:39:27 +0200

    Added trigger type configuration ability in gpiointr_config.

--- src/gpiointr_config.c
+++ src/gpiointr_config.c
@@ -42,6 +42,41 @@ int main(int argc, char *argv[])
 	if (errno != 0)
 		err(EXIT_FAILURE, "Invalid pin number");
 
+
+	if (argc == 1) {
+		printf("%s: No trigger type specified.\n", getprogname());
+		return EXIT_FAILURE;
+	}
+
+	if (strnlen(argv[1], 2) < 2) {
+		printf("%s: Invalid trigger type (argument too short).\n", getprogname());
+		return EXIT_FAILURE;
+	}
+
+	switch((argv[1][0] << 8) + argv[1][1]) {
+		case ('n' << 8) + 'o':
+			intr_config.gp_intr_flags = GPIO_INTR_NONE;
+			break;
+		case ('l' << 8) + 'l':
+			intr_config.gp_intr_flags = GPIO_INTR_LEVEL_LOW;
+			break;
+		case ('l' << 8) + 'h':
+			intr_config.gp_intr_flags = GPIO_INTR_LEVEL_HIGH;
+			break;
+		case ('e' << 8) + 'r':
+			intr_config.gp_intr_flags = GPIO_INTR_EDGE_RISING;
+			break;
+		case ('e' << 8) + 'f':
+			intr_config.gp_intr_flags = GPIO_INTR_EDGE_FALLING;
+			break;
+		case ('e' << 8) + 'b':
+			intr_config.gp_intr_flags = GPIO_INTR_EDGE_BOTH;
+			break;
+		default:
+			printf("%s: Invalid trigger type.\n", getprogname());
+			return EXIT_FAILURE;
+	}
+
 	fd = open(file, O_RDONLY);
 	if(fd == -1)
 		err(EXIT_FAILURE, "Cannot open %s", file);
