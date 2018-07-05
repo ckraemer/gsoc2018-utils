bb08b301e0eabb0e690126cc1e135de64c859f70
Christian Krämer <uddka@student.kit.edu>
2018-07-05 09:44:28 +0200

    Updated some further utilities.
    Enabled pin distinction in gpiointr_aio_read, gpiointr_kqueue,
    gpiointr_poll and gpiointr_select.

--- src/gpiointr_aio_read.c
+++ src/gpiointr_aio_read.c
@@ -8,9 +8,36 @@
 #include <errno.h>
 #include <err.h>
 
+#include <libgpio.h>
+
 void usage()
 {
-	fprintf(stderr, "usage: %s [-f gpiointrdev] [-s]\n", getprogname());
+	fprintf(stderr, "usage: %s [-f gpiointrdev] [-s] pin intr-config\n\n", getprogname());
+	fprintf(stderr, "Possible options for intr-config:\n\n");
+	fprintf(stderr, "no\tno interrupt\n");
+	fprintf(stderr, "ll\t level low\n");
+	fprintf(stderr, "lh\t level high\n");
+	fprintf(stderr, "er\t edge rising\n");
+	fprintf(stderr, "ef\t edge falling\n");
+	fprintf(stderr, "eb\t edge both\n");
+}
+
+static const char* intr_mode_to_str(uint32_t intr_mode)
+{
+	switch (intr_mode) {
+	case GPIO_INTR_LEVEL_LOW:
+		return "low level";
+	case GPIO_INTR_LEVEL_HIGH:
+		return "high level";
+	case GPIO_INTR_EDGE_RISING:
+		return "rising edge";
+	case GPIO_INTR_EDGE_FALLING:
+		return "falling edge";
+	case GPIO_INTR_EDGE_BOTH:
+		return "both edges";
+	default:
+		return "invalid mode";
+	}
 }
 
 int main(int argc, char *argv[])
@@ -18,10 +45,11 @@ int main(int argc, char *argv[])
 	int ch;
 	char *file = "/dev/gpiointr0";
 	bool loop = true;
-	int fd;
+	int handle;
 	char buffer[1024];
 	struct aiocb iocb;
 	int res;
+	gpio_intr_config_t intr_config;
 
 	while ((ch = getopt(argc, argv, "f:s")) != -1) {
 		switch (ch) {
@@ -39,13 +67,68 @@ int main(int argc, char *argv[])
 	argv += optind;
 	argc -= optind;
 
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
+	if (argc == 0) {
+		fprintf(stderr, "%s: No pin number specified.\n", getprogname());
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	errno = 0;
+	intr_config.g_pin = strtol(argv[0], NULL, 10);
+	if (errno != 0) {
+		warn("Invalid pin number");
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	if (argc == 1) {
+		fprintf(stderr, "%s: No trigger type specified.\n", getprogname());
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	if (strnlen(argv[1], 2) < 2) {
+		fprintf(stderr, "%s: Invalid trigger type (argument too short).\n", getprogname());
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	switch((argv[1][0] << 8) + argv[1][1]) {
+	case ('n' << 8) + 'o':
+		intr_config.g_intr_flags = GPIO_INTR_NONE;
+		break;
+	case ('l' << 8) + 'l':
+		intr_config.g_intr_flags = GPIO_INTR_LEVEL_LOW;
+		break;
+	case ('l' << 8) + 'h':
+		intr_config.g_intr_flags = GPIO_INTR_LEVEL_HIGH;
+		break;
+	case ('e' << 8) + 'r':
+		intr_config.g_intr_flags = GPIO_INTR_EDGE_RISING;
+		break;
+	case ('e' << 8) + 'f':
+		intr_config.g_intr_flags = GPIO_INTR_EDGE_FALLING;
+		break;
+	case ('e' << 8) + 'b':
+		intr_config.g_intr_flags = GPIO_INTR_EDGE_BOTH;
+		break;
+	default:
+		fprintf(stderr, "%s: Invalid trigger type.\n", getprogname());
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	handle = gpio_intr_open_device(file);
+	if(handle == GPIO_INVALID_HANDLE)
 		err(EXIT_FAILURE, "Cannot open %s", file);
+
+	res = gpio_intr_pin_set_flags(handle, &intr_config);
+	if(res < 0)
+		err(EXIT_FAILURE, "configuration of pin %d on %s failed", intr_config.g_pin, file);
 
 	bzero(&iocb, sizeof(iocb));
 
-	iocb.aio_fildes = fd;
+	iocb.aio_fildes = handle;
 	iocb.aio_nbytes = 1;
 	iocb.aio_offset = 0;
 	iocb.aio_buf = buffer;
--- src/gpiointr_poll.c
+++ src/gpiointr_poll.c
@@ -10,9 +10,36 @@
 
 #include <sys/poll.h>
 
+#include <libgpio.h>
+
 void usage()
 {
-	fprintf(stderr, "usage: %s [-f gpiointrdev] [-s] [-t timeout]\n", getprogname());
+	fprintf(stderr, "usage: %s [-f gpiointrdev] [-s] [-t timeout] pin intr-config\n\n", getprogname());
+	fprintf(stderr, "Possible options for intr-config:\n\n");
+	fprintf(stderr, "no\tno interrupt\n");
+	fprintf(stderr, "ll\t level low\n");
+	fprintf(stderr, "lh\t level high\n");
+	fprintf(stderr, "er\t edge rising\n");
+	fprintf(stderr, "ef\t edge falling\n");
+	fprintf(stderr, "eb\t edge both\n");
+}
+
+static const char* intr_mode_to_str(uint32_t intr_mode)
+{
+	switch (intr_mode) {
+	case GPIO_INTR_LEVEL_LOW:
+		return "low level";
+	case GPIO_INTR_LEVEL_HIGH:
+		return "high level";
+	case GPIO_INTR_EDGE_RISING:
+		return "rising edge";
+	case GPIO_INTR_EDGE_FALLING:
+		return "falling edge";
+	case GPIO_INTR_EDGE_BOTH:
+		return "both edges";
+	default:
+		return "invalid mode";
+	}
 }
 
 static const char *poll_event_to_str(short event)
@@ -67,9 +94,10 @@ int main(int argc, char *argv[])
 	char *file = "/dev/gpiointr0";
 	bool loop = true;
 	int timeout = INFTIM;
-	int fd;
+	int handle;
 	struct pollfd fds;
 	int res;
+	gpio_intr_config_t intr_config;
 
 	while ((ch = getopt(argc, argv, "f:st:")) != -1) {
 		switch (ch) {
@@ -96,13 +124,68 @@ int main(int argc, char *argv[])
 	argv += optind;
 	argc -= optind;
 
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
+	if (argc == 0) {
+		fprintf(stderr, "%s: No pin number specified.\n", getprogname());
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	errno = 0;
+	intr_config.g_pin = strtol(argv[0], NULL, 10);
+	if (errno != 0) {
+		warn("Invalid pin number");
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	if (argc == 1) {
+		fprintf(stderr, "%s: No trigger type specified.\n", getprogname());
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	if (strnlen(argv[1], 2) < 2) {
+		fprintf(stderr, "%s: Invalid trigger type (argument too short).\n", getprogname());
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	switch((argv[1][0] << 8) + argv[1][1]) {
+	case ('n' << 8) + 'o':
+		intr_config.g_intr_flags = GPIO_INTR_NONE;
+		break;
+	case ('l' << 8) + 'l':
+		intr_config.g_intr_flags = GPIO_INTR_LEVEL_LOW;
+		break;
+	case ('l' << 8) + 'h':
+		intr_config.g_intr_flags = GPIO_INTR_LEVEL_HIGH;
+		break;
+	case ('e' << 8) + 'r':
+		intr_config.g_intr_flags = GPIO_INTR_EDGE_RISING;
+		break;
+	case ('e' << 8) + 'f':
+		intr_config.g_intr_flags = GPIO_INTR_EDGE_FALLING;
+		break;
+	case ('e' << 8) + 'b':
+		intr_config.g_intr_flags = GPIO_INTR_EDGE_BOTH;
+		break;
+	default:
+		fprintf(stderr, "%s: Invalid trigger type.\n", getprogname());
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	handle = gpio_intr_open_device(file);
+	if(handle == GPIO_INVALID_HANDLE)
 		err(EXIT_FAILURE, "Cannot open %s", file);
 
-	fds.fd = fd;
+	fds.fd = handle;
 	fds.events = POLLIN | POLLRDNORM;
 	fds.revents = 0;
+
+	res = gpio_intr_pin_set_flags(handle, &intr_config);
+	if(res < 0)
+		err(EXIT_FAILURE, "configuration of pin %d on %s failed", intr_config.g_pin, file);
 
 	do {
 
