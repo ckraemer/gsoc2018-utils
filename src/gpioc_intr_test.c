8a72f37089c2945d83f545464bd3fe69183e0c47
Christian Krämer <uddka@student.kit.edu>
2018-07-20 14:07:21 +0200

    Added poll() support in gpioc_intr_test.

--- src/gpioc_intr_test.c
+++ src/gpioc_intr_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
@@ -7,13 +8,16 @@
 #include <errno.h>
 #include <err.h>
 
+#include <sys/poll.h>
+
 #include <libgpio.h>
 
 void usage()
 {
-	fprintf(stderr, "usage: %s [-f ctldev] [-m method] [-s] pin intr-config [pin intr-config ...]\n\n", getprogname());
+	fprintf(stderr, "usage: %s [-f ctldev] [-m method] [-s] [-t timeout] pin intr-config [pin intr-config ...]\n\n", getprogname());
 	fprintf(stderr, "Possible options for method:\n\n");
-	fprintf(stderr, "r\tread (default)\n\n");
+	fprintf(stderr, "r\tread (default)\n");
+	fprintf(stderr, "p\tpoll\n\n");
 	fprintf(stderr, "Possible options for intr-config:\n\n");
 	fprintf(stderr, "no\tno interrupt\n");
 	fprintf(stderr, "ll\t level low\n");
@@ -41,6 +45,52 @@ static const char* intr_mode_to_str(uint32_t intr_mode)
 	}
 }
 
+static const char *poll_event_to_str(short event)
+{
+	switch (event) {
+	case POLLIN:
+		return "POLLIN";
+	case POLLPRI:
+		return "POLLPRI:";
+	case POLLOUT:
+		return "POLLOUT:";
+	case POLLRDNORM:
+		return "POLLRDNORM";
+	case POLLRDBAND:
+		return "POLLRDBAND";
+	case POLLWRBAND:
+		return "POLLWRBAND";
+	case POLLINIGNEOF:
+		return "POLLINIGNEOF";
+	case POLLERR:
+		return "POLLERR";
+	case POLLHUP:
+		return "POLLHUP";
+	case POLLNVAL:
+		return "POLLNVAL";
+	default:
+		return "unknown event";
+	}
+}
+
+void print_events(short event)
+{
+	short curr_event = 0;
+	bool first = true;
+
+	for (int i = 0; i <= sizeof(short) * CHAR_BIT - 1; i++) {
+		curr_event = 1 << i;
+		if ((event & curr_event) == 0)
+			continue;
+		if (!first) {
+			printf(" | ");
+		} else {
+			first = false;
+		}
+		printf("%s", poll_event_to_str(curr_event));
+	}
+}
+
 void run_read(bool loop, int handle, char *file, char *buffer)
 {
 	ssize_t res;
@@ -54,18 +104,45 @@ void run_read(bool loop, int handle, char *file, char *buffer)
 	} while (loop);
 }
 
+void run_poll(bool loop, int handle, char *file, int timeout)
+{
+	struct pollfd fds;
+	int res;
+
+        fds.fd = handle;
+        fds.events = POLLIN | POLLRDNORM;
+        fds.revents = 0;
+
+	do {
+		res = poll(&fds, 1, timeout);
+		if (res < 0) {
+			err(EXIT_FAILURE, "Cannot poll() %s", file);
+		} else if (res == 0) {
+			printf("%s: poll() timed out on %s\n", getprogname(), file);
+		} else {
+			printf("%s: poll() returned %i (revents: ", getprogname(), res);
+			print_events(fds.revents);
+			printf(") on %s\n", file);
+			if (fds.revents & (POLLHUP | POLLERR)) {
+				err(EXIT_FAILURE, "Recieved POLLHUP or POLLERR on %s", file);
+			}
+		}
+	} while (loop);
+}
+
 int main(int argc, char *argv[])
 {
 	int ch;
 	char *file = "/dev/gpioc0";
 	char method = 'r';
 	bool loop = true;
+	int timeout = INFTIM;
 	int handle;
 	char buffer[1024];
 	int res;
 	gpio_config_t pin_config;
 
-	while ((ch = getopt(argc, argv, "f:m:s")) != -1) {
+	while ((ch = getopt(argc, argv, "f:m:st:")) != -1) {
 		switch (ch) {
 		case 'f':
 			file = optarg;
@@ -75,6 +152,15 @@ int main(int argc, char *argv[])
 			break;
 		case 's':
 			loop = false;
+			break;
+		case 't':
+			errno = 0;
+			timeout = strtol(optarg, NULL, 10);
+			if (errno != 0) {
+				warn("Invalid timeout value");
+				usage();
+				return EXIT_FAILURE;
+			}
 			break;
 		default:
 			usage();
@@ -149,6 +235,7 @@ int main(int argc, char *argv[])
 
 		pin_config.g_flags |= GPIO_PIN_INPUT | GPIO_PIN_PULLUP;
 
+
 		res = gpio_pin_set_flags(handle, &pin_config);
 		if(res < 0)
 			err(EXIT_FAILURE, "configuration of pin %d on %s failed (flags=%d)", pin_config.g_pin, file, pin_config.g_flags);
@@ -158,6 +245,8 @@ int main(int argc, char *argv[])
 	switch (method) {
 	case 'r':
 		run_read(loop, handle, file, buffer);
+	case 'p':
+		run_poll(loop, handle, file, timeout);
 	default:
 		fprintf(stderr, "%s: Unknown method.\n", getprogname());
 		usage();
